e52542b98e8822529d9dbbebc074348e55b53a64
Pavel Potemkin <p.potemkin@legacystudio.ru>
2022-03-05 15:19:56 +0500

    Сделал меню, добавил примерное расположение элементов на странице

---
+++ src/components/nav.js
@@ -0,0 +1,29 @@
+export default class Nav {
+    constructor(el) {
+        this.el = el
+        this.isOpen = false
+    }
+    
+    toggleNav() {
+        this.isOpen ? this.hideNav() : this.openNav() 
+    }
+    
+    openNav() {
+        this.el.classList.add('nav_open')
+        this.isOpen = true
+    }
+    
+    hideNav() {
+        this.el.classList.remove('nav_open')
+        this.isOpen = false
+    }
+    
+    openMenuItem(item) {
+        item.classList.add('nav__item_open')
+    }
+
+    hideMenuItem(hideBtn) {
+        const item = hideBtn.closest('[data-nav-item]')        
+        item.classList.remove('nav__item_open')
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './assets/style/index.scss'
 import Select from './components/select'
 import Accordion from "@/components/accordion";
+import Nav from "@/components/nav";
 
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
@@ -46,4 +47,31 @@ function init() {
     }
 
     filter()
+    
+    if (window.innerWidth <= 1024) {
+        const nav = new Nav(document.getElementById('nav'))
+        
+        const burger = document.querySelector('[data-toggle-nav]')
+        burger.addEventListener('click', () => nav.toggleNav())
+        
+        const closeIcon = document.querySelector('[data-hide-nav]')
+        closeIcon.addEventListener('click', () => nav.hideNav())
+        
+        const items = document.querySelectorAll('[data-nav-item]')
+        items.forEach(item => {
+            item.addEventListener('click', () => nav.openMenuItem(item))
+        })
+        
+        const hideNavItems = document.querySelectorAll('[data-nav-item-hide]')
+        hideNavItems.forEach(hideNavItem => {
+            hideNavItem.addEventListener('click', () => nav.hideMenuItem(hideNavItem))
+        })
+        
+        const subcategories = document.querySelectorAll('[data-nav-subitems]')
+        subcategories.forEach(subcategory => {
+            subcategory.addEventListener('click', event => {
+                event.stopPropagation()
+            })
+        })
+    }
 }
