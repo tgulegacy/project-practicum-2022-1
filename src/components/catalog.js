36c2c70e5800d517e9d39ac90fcdf093ce226d8c
Pavel <p.potemkin.work@gmail.com>
2022-03-19 13:05:57 +0500

    Добавил рендер каталога

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -1,61 +1,52 @@
-import navData from "@/config/catalog-data";
-import catalogData from "@/config/catalog-data";
-
 export default class Catalog {
     constructor(el) {
-        this.el = document.getElementById(el);
-        this.init();
+        this.el = el
     }
 
-    init() {
-        this.render();
+    renderItems(items) {
+        let itemsDom = ''
+
+        items.forEach(item => {
+            itemsDom += this.renderItem(item)
+        })
+
+        this.el.innerHTML = itemsDom
     }
 
-    render() {
-        let container = `
-			<div class="catalog__items">
-		`;
+    renderItem(item) {
+        return `
+        <div class="product-card">
+            <div class="product-card__inner">
+                <div class="product-card__controls">
+                    <svg class="svg-grey" width="24" height="22">
+                        <use href="#hearth"></use>
+                    </svg>
 
-        catalogData.forEach( item => {
-            const name = item.name;
-            const price = item.price;
-            const image = item.image;
+                    <div class="product-card__basket ${item.inBasket ? 'product-card__basket_active' : ''}">
+                        <svg class="svg-primary" width="20" height="20">
+                            <use href="#basket"></use>
+                        </svg>
+                    </div>
+                </div>
 
-            let domElem = `
-                    
-                                <div class="catalog__item">
-                                    <div style="background: #FFFFFF;padding: 24px 
-                                    24px 145px 24px;border: 1px solid #E9E9E9;border-radius: 2px;">
-                                        <div class="catalog__svgg">
-                                            <svg class="catalog__svg" width="20" height="20">
-                                                <use href="#basket"></use>
-                                            </svg>
-                                            <svg class="catalog__svg1" width="20" height="20">
-                                                <use href="#basket"></use>
-                                            </svg>
-                                        </div>
-                
-                                        <img src="${image}" alt=""
-                                             style="width: 100%;height:100%;object-fit: 
-                                                 contain;" height="238">
-                                        <div class="catalog__np">
-                                            <div class="catalog__name">
-                                                ${name}
-                                            </div>
-                                            <div class="catalog__price">
-                                                ${price}
-                                            </div>
-                                        </div>
-                                     </div>
-                                </div>
-			`;
-            
+                <img class="product-card__image"
+                     src="${item.image}"
+                     alt="Изображение">
 
-            container += domElem;
-        } );
-        container += '</div>';
+                <div class="product-card__description">
+                    ${item.description}
+                </div>
 
-        this.el.innerHTML = container;
+                <div class="product-card__price">
+                    <span>
+                        ${item.price}
+                    </span>
+
+                    <svg class="svg-primary" width="17" height="17">
+                        <use href="#rub"></use>
+                    </svg>
+                </div>
+            </div>
+        </div>`
     }
-    
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import Select from './components/select'
 import Accordion from "@/components/accordion";
 import MobileNav from "@/components/mobile-nav";
 import Nav from '@/components/nav';
-import Catalog from '@/components/catalog';
+import getCatalogItems from "@/api/getCatalogItems";
+import Catalog from "@/components/catalog";
+import getFilterItems from "@/api/getFilterItems";
 
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
@@ -13,19 +15,21 @@ if (document.readyState === 'loading') {
     init();
 }
 
-function init() {
+async function init() {
     new Nav('nav');
     const sortEl = document.getElementById('sort')
     const sortCallback = (item) => {
         console.log(item)
     }
-    new Catalog('cat');
-    
     new Select({
         el: sortEl,
         onChange: sortCallback,
         cookieName: 'catalog-sort'
     })
+
+    const filterItems = await getFilterItems()
+    console.log(filterItems)
+    //render filter items
 
     function filter() {
         const accordions = []
@@ -79,4 +83,11 @@ function init() {
             })
         })
     }
+
+    const catalog = new Catalog(document.getElementById('catalog-items'))
+
+    const catalogItems = await getCatalogItems()
+
+    catalog.renderItems(catalogItems)
+
 }
